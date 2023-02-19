0b0a68356773843c833d9608546124a2447f0eea
crewsbd <bdcrews@protonmail.com>
2023-02-18 20:53:56 -0800

    Lazy loading and responsive image size headache

--- chamber/scripts/datestamp.js
+++ chamber/scripts/datestamp.js
@@ -1,6 +1,7 @@
 const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
 
+
 document.querySelector("#year-field").innerText = currentYear;
 document.querySelector("#last-update-field").innerText = document.lastModified;
 document.querySelector("#current-date").textContent = Intl.DateTimeFormat("en-UK", {
