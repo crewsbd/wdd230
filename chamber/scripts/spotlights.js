b6b4fdd0c7075a556ebf6763746207cc4a819bfd
crewsbd <bdcrews@protonmail.com>
2023-03-18 14:52:46 -0700

    Added alt text to spotlight images

--- chamber/scripts/spotlights.js
+++ chamber/scripts/spotlights.js
@@ -11,44 +11,13 @@ async function spotlights() {
     const goldBusinesses = getSpotlights(businesses);
     const randomSort = getRandomItems(goldBusinesses, 3);
 
-    spotlight1.innerHTML = `<h2>${randomSort[0].name}</h2><img src="${randomSort[0].imgurl}"><p>${randomSort[0].street1}<br>${randomSort[0].street2}<br><a href="http://${randomSort[0].url}" target="_blank">${breakURL(randomSort[0].url)}</a></p>`;
-    spotlight2.innerHTML = `<h2>${randomSort[1].name}</h2><img src="${randomSort[1].imgurl}"><p>${randomSort[1].street1}<br>${randomSort[1].street2}<br><a href="http://${randomSort[1].url}" target="_blank">${breakURL(randomSort[1].url)}</a></p>`;
-    spotlight3.innerHTML = `<h2>${randomSort[2].name}</h2><img src="${randomSort[2].imgurl}"><p>${randomSort[2].street1}<br>${randomSort[2].street2}<br><a href="http://${randomSort[2].url}" target="_blank">${breakURL(randomSort[2].url)}</a></p>`;
+    spotlight1.innerHTML = `<h2>${randomSort[0].name}</h2><img src="${randomSort[0].imgurl}" alt="${randomSort[0].name}"><p>${randomSort[0].street1}<br>${randomSort[0].street2}<br><a href="http://${randomSort[0].url}" target="_blank">${breakURL(randomSort[0].url)}</a></p>`;
+    spotlight2.innerHTML = `<h2>${randomSort[1].name}</h2><img src="${randomSort[1].imgurl}" alt="${randomSort[1].name}"><p>${randomSort[1].street1}<br>${randomSort[1].street2}<br><a href="http://${randomSort[1].url}" target="_blank">${breakURL(randomSort[1].url)}</a></p>`;
+    spotlight3.innerHTML = `<h2>${randomSort[2].name}</h2><img src="${randomSort[2].imgurl}" alt="${randomSort[2].name}"><p>${randomSort[2].street1}<br>${randomSort[2].street2}<br><a href="http://${randomSort[2].url}" target="_blank">${breakURL(randomSort[2].url)}</a></p>`;
 
     spotlight1.parentElement.style.backgroundImage = `radial-gradient(rgba(0,0,0,0) 70%, rgba(0,0,0,.1)), url(images/directory/${randomSort[0].bg}bg_400.webp)`;
     spotlight2.parentElement.style.backgroundImage = `radial-gradient(rgba(0,0,0,0) 70%, rgba(0,0,0,.1)), url(images/directory/${randomSort[1].bg}bg_400.webp)`;
     spotlight3.parentElement.style.backgroundImage = `radial-gradient(rgba(0,0,0,0) 70%, rgba(0,0,0,.1)), url(images/directory/${randomSort[2].bg}bg_400.webp)`;
-
-
-    //Future reference---
-    /*businesses.forEach(business => {
-
-        const img = document.createElement("img");
-        img.setAttribute("src", business.imgurl);
-        img.setAttribute("alt", business.name);
-        img.setAttribute("height", "100");
-
-        const address = document.createElement("div");
-        address.innerHTML = `${business.street1}<br>${business.street2}`;
-
-        const phone = document.createElement("div");
-        phone.innerHTML = `${business.phone}`;
-
-        const busurl = document.createElement("div");
-        const busurllink = document.createElement("a");
-        busurllink.innerHTML = `<a href="http:////${business.url}" target="_blank">${business.url}</a>`
-        busurl.appendChild(busurllink);
-
-        const card = document.createElement("div");
-        card.classList.add("card");
-        card.appendChild(img);
-        card.appendChild(address);
-        card.appendChild(phone);
-        card.appendChild(busurl);
-
-        directoryContent.appendChild(card);
-    } 
-    ); */
 }
 function getRandomItems(items, count) {
     const shuffled = [...items].sort(() => 0.5 - Math.random());
